9a352e3b2c2e23b17191459c98bc1f4dc659390e
Steve Vander Stouw <svanderstouw@gmail.com>
2018-09-17 18:28:31 -0400

    Added modal

--- js/script.js
+++ js/script.js
@@ -1,17 +1,62 @@
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
+(function () {
 
-// Get the navbar
-let navbar = document.getElementById("navbar");
+    setUpPage();
 
-// Get the offset position of the navbar
-let sticky = navbar.offsetTop;
+    function setUpPage() {
+        stickyNavBar();
+        noDragImgs();
+        addModalEventListeners();
+    }
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+    //Make Nav Bar sticky
+    function stickyNavBar() {
+         
+        window.onscroll = function () { checkNav() };
+
+        let navbar = document.getElementById("navbar");
+        let sticky = navbar.offsetTop;
+
+        // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+        function checkNav() {
+            if (window.pageYOffset >= sticky) {
+                navbar.classList.add("sticky")
+            } else {
+                navbar.classList.remove("sticky");
+            }
+        }
+    }
+
+    //Make all images not draggable
+    function noDragImgs() {
+        const allImgs = document.getElementsByTagName('img');
+
+        for (eachImg of allImgs) {
+            eachImg.draggable = false;
+        }
+    }
+
+    // Modal can be opened
+    function addModalEventListeners() {
+
+        const modalTriggerElement = document.querySelector('#modal-trigger');
+
+        const modalTriggerClickHandler = function () {
+            document.body.classList.add('modal-open');
+        };
+
+        modalTriggerElement.addEventListener('click', modalTriggerClickHandler);
+
+        // Modal can be closed
+        const modalCloseButton = document.querySelector('#modal a');
+        const modalBackground = document.querySelector('#backdrop');
+
+        const modalCloseButtonClickHandler = function () {
+            document.body.classList.remove('modal-open');
+        };
+
+        modalCloseButton.addEventListener('click', modalCloseButtonClickHandler);
+        modalBackground.addEventListener('click', modalCloseButtonClickHandler);
+    };
+
+
+})();
