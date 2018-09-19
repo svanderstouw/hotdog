e7ed90743080eaeeb7925920b2240f22606dd474
Steve Vander Stouw <svanderstouw@gmail.com>
2018-09-19 13:06:30 -0400

    Added responsiveness to webpage

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dirty Dogs Webpage
+
+This project creates a responsive webpage for Dirty Dogs Restaurant utilizing HTML5, CSS3/Flexbox/Grid, and JavaScript.
--- js/script.js
+++ js/script.js
@@ -16,7 +16,6 @@
         let navbar = document.getElementById("navbar");
         let sticky = navbar.offsetTop;
 
-        // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
         function checkNav() {
             if (window.pageYOffset >= sticky) {
                 navbar.classList.add("sticky")
